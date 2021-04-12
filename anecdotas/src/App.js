e05448ba167dbc00d8550e5dab8b7d65898a78c2
Eduardo <xeduardox54@hotmail.com>
2021-04-12 16:52:13 -0500

    Punto 2

--- anecdotas/src/App.js
+++ anecdotas/src/App.js
@@ -1,23 +1,50 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [selected, setSelected] = useState("")
+  const [selected, setSelected] = useState(anecdotes[Math.floor(Math.random() * anecdotes.length)])
+  const [votes, setVotes] = useState(0)
+
+  function changeAnecdote() {
+    const anecdote = anecdotes[Math.floor(Math.random() * anecdotes.length)];
+    setSelected(anecdote);
+    setVotes(anecdote.votes);
+  }
 
   return (
-    <div>
-      <h4>{selected}</h4>
-      <button onClick={()=>{setSelected(anecdotes[Math.floor(Math.random() * anecdotes.length)])}} >Mostrar anécdota</button>
+    <div> 
+      <h4>{selected.anecdote}</h4>
+      <h3>Has {votes}</h3>
+      <button onClick={()=>{selected.votes++;setVotes(selected.votes)}}>Vote</button>
+      <button onClick={changeAnecdote} >Next Anecdote</button>
     </div>
   )
 }
 
 const anecdotes = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  {
+    anecdote: 'If it hurts, do it more often',
+    votes: 0,
+  },
+  { 
+    anecdote: 'Adding manpower to a late software project makes it later!',
+    votes: 0,
+  },
+  {
+    anecdote: 'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    votes: 0,
+  },
+  {
+    anecdote: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    votes: 0,
+  },
+  {
+    anecdote: 'Premature optimization is the root of all evil.',
+    votes: 0,
+  },
+  {
+    anecdote: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    votes: 0,
+  },
 ]
 
 export default App;
